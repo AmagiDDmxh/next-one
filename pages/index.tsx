78c7acb63cf93aaaf607190041761a8aacd70e83
amagi <amagiddmxh@qq.com>
2021-04-14 12:32:00 +0800

    Update template to use primary color

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export const Home = (): JSX.Element => (
             width={20}
             height={20}
             fill="currentColor"
-            className="text-violet-600"
+            className="text-green-600"
           >
             <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
           </svg>
@@ -46,7 +46,7 @@ export const Home = (): JSX.Element => (
         </p>
         <button
           type="button"
-          className="bg-violet-100 text-violet-700 text-base font-semibold px-6 py-2 rounded-lg"
+          className="bg-green-600 text-white text-base font-semibold px-6 py-2 rounded-lg focus:outline-none"
         >
           Check availability
         </button>
