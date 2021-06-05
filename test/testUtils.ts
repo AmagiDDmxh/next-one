30e15a3c1b07cc7d7c42cec6c7437b08448da79e
AmagiDDmxh <amagiddmxh@qq.com>
2021-06-05 23:08:49 +0800

    Add test typings

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,10 +1,17 @@
-import { render } from '@testing-library/react'
+import {
+  queries,
+  Queries,
+  render,
+  RenderOptions,
+  RenderResult,
+} from '@testing-library/react'
+import { FC, ReactElement } from 'react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
-const Providers = ({ children }) => {
-  return children
+const Providers: FC = ({ children }) => {
+  return children as ReactElement
   // return (
   //   <ThemeProvider theme="light">
   //     <TranslationProvider messages={defaultStrings}>
@@ -14,7 +21,15 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui, options = {}) =>
+export type CustomRender = <
+  Q extends Queries = typeof queries,
+  Container extends Element | DocumentFragment = HTMLElement
+>(
+  ui: React.ReactElement,
+  options: RenderOptions<Q, Container>
+) => RenderResult<Q, Container>
+
+const customRender: CustomRender = (ui, options = {}) =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
