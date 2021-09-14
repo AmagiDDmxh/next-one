810288d36be21a79b735a1c9e533b8cc03e305a5
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-15 02:38:29 +0800

    Use raw translations instead of ast

--- locales/index.ts
+++ locales/index.ts
@@ -1,8 +1,7 @@
-import { MessageFormatElement } from '@formatjs/icu-messageformat-parser'
 import enUS from './en-US.json'
 import zhCN from './zh-CN.json'
 
 export default {
   'en-US': enUS,
   'zh-CN': zhCN,
-} as Record<string, Record<string, MessageFormatElement[]>>
+} as const
